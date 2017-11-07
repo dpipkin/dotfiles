96bd2f8d541e1c0d7ba22005f9f6dbd4df209f1a
Daniel Pipkin <daniel.pipkin@mx.com>
2017-11-07 14:56:23 -0700

    wrong place for these functions

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,11 +1,3 @@
 function config
 	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv;
 end
-
-function bx
-  bundle exec $argv;
-end
-
-function bigheap
-  set -lx JRUBY_OPTS='-J-Xmx2g' $argv;
-end
