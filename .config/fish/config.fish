d581f40bfedb301373105fcc8365a847fe2483de
Daniel Pipkin <daniel.pipkin@mx.com>
2017-11-07 14:57:02 -0700

    setup anaconda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ rvm default
 
 # Set my editor to vim
 set -x EDITOR vim
+
+# setup miniconda to work
+set -x PATH $PATH /Users/danielpipkin/miniconda3/bin
+source (conda info --root)/etc/fish/conf.d/conda.fish
