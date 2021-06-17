47a73efe8368c78d5bd3266a1a34d1fc45a51cba
Daniel Pipkin <daniel.pipkin@mx.com>
2021-06-17 11:33:11 -0700

    Cleaning up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
-# setup rvm to work
-rvm default
-
 # Set my editor to vim
 set -x EDITOR vim
 
-# setup miniconda to work
-set -x PATH $PATH /Users/danielpipkin/miniconda3/bin
-source (conda info --root)/etc/fish/conf.d/conda.fish
